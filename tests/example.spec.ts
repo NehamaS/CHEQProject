109e927d54c3acd84fa296827e3f11d4660f1d2a
nehama stefanski <nehama.stefanski@mavenir.com>
2022-07-25 13:38:29 +0300

    add files

---
+++ tests/example.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+import { fileImport, } from "../common/Helper"
+import {findMaliciousWithSpecificIP, findSpecificIPsList} from "../common/malicious.find"
+
+let data
+test.beforeEach(async () => {
+  data=await fileImport("data.xlsx")
+});
+
+test('check if there is malicious bot match 140.235.248.3 IP in Excel', async () => {
+
+   await findMaliciousWithSpecificIP(data,'61.247.133.200')
+});
+
+test('check if all malicious data in centers list are in Excel', async () => {
+   const ipList=['61.247.133.200','241.81.13.96']
+   await findSpecificIPsList(data,ipList)
+
+});
+
